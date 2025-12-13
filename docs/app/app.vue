c639db5dc6ce1460b5b0c81ccec99ca475c3b9d9
onmax <maximogarciamtnez@gmail.com>
2025-12-13 13:12:18 +0100

    docs: add early development banner

---
+++ docs/app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UBanner icon="i-lucide-construction" title="This library is in early development. Expect breaking changes." color="warning" />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
